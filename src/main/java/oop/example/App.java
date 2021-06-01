9a8b08b7693f49fe0ef0e3ae19c664ff6722ee0e
MicahPB01 <micahpuccioball@gmail.com>
2021-05-31 21:17:10 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,69 @@
+package oop.example;
+
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        int userInput;
+        String month = null;
+
+        System.out.print("Please enter the number of this month: ");
+        userInput = Integer.parseInt(input.nextLine());
+
+        switch(userInput)   {
+            case(1):   {
+                month = "January";
+                break;
+            }
+            case(2):   {
+                month = "February";
+                break;
+            }
+            case(3):   {
+                month = "March";
+                break;
+            }
+            case(4):   {
+                month = "April";
+                break;
+            }
+            case (5):   {
+                month = "May";
+                break;
+            }
+            case(6):   {
+                month = "June";
+                break;
+            }
+            case(7):   {
+                month = "July";
+                break;
+            }
+            case(8):   {
+                month = "August";
+                break;
+            }
+            case(9):   {
+                month = "September";
+                break;
+            }
+            case(10):   {
+                month = "October";
+                break;
+            }
+            case(11):   {
+                month = "November";
+                break;
+            }
+            case(12):   {
+                month = "December";
+                break;
+            }
+
+        }
+        System.out.printf("The name of this month is %s.\n", month);
+    }
+}
